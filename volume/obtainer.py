eae380d1923175b6a45bd5d5e46871a4147fc1e0
Naphat Amundsen <naphat@live.no>
2022-01-05 19:16:29 +0100

    update dockerfile

---
+++ volume/obtainer.py
@@ -0,0 +1,3 @@
+import requests
+
+URL = 'https://www.finn.no/api/search-qf?searchkey=SEARCH_ID_REALESTATE_HOMES&lat=60.386516342860716&lon=5.32861852263332&radius=5000&sort=PUBLISHED_DESC&location=1.22046.20220'
